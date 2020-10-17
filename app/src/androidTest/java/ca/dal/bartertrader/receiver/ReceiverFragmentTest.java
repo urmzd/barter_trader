085c13c74ee592d5fd7fdfe1289a72cf0b15662c
Celeste MacNeil <celestemacneil@dal.ca>
2020-10-17 17:00:15 -0300

    Add SwitchRoles() test

--- app/src/androidTest/java/ca/dal/bartertrader/receiver/ReceiverFragmentTest.java
+++ app/src/androidTest/java/ca/dal/bartertrader/receiver/ReceiverFragmentTest.java
@@ -1,5 +1,63 @@
+package ca.dal.bartertrader.receiver;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.testing.TestNavHostController;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.truth.view.PointerPropertiesSubject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import ca.dal.bartertrader.R;
+import ca.dal.bartertrader.provider.DummyProviderFragment;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import static org.junit.Assert.*;
 
 public class ReceiverFragmentTest {
+    TestNavHostController navController;
 
+    @Before
+    public void setUp() throws Exception {
+        navController = new TestNavHostController(
+                ApplicationProvider.getApplicationContext());
+        navController.setGraph(R.navigation.nav_graph);
+        navController.setCurrentDestination(R.id.ReceiverFragment);
+    }
+
+    @Test
+    public void testSwitchRoles()
+    {
+
+
+        // Create a graphical FragmentScenario for the TitleScreen
+        FragmentScenario<ReceiverFragment> receiverScenario =
+                FragmentScenario.launchInContainer(ReceiverFragment.class);
+
+        // Set the NavController property on the fragment
+        receiverScenario.onFragment(fragment ->
+                Navigation.setViewNavController(fragment.requireView(), navController)
+        );
+
+        onView(withId(R.id.switchRolesButton))
+                .perform(click());
+
+        assertEquals(navController.getCurrentDestination().getId(),R.id.DummyProviderFragment);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
