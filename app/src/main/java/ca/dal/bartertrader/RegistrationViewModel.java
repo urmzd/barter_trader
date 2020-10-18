cdeb9464ffb0bc3a790df68e5b97bf6fa43e1854
jaydenm <jayden.macdonald@dal.ca>
2020-10-18 19:31:37 -0300

    Add more placeholder functions to the viewmodel

--- app/src/main/java/ca/dal/bartertrader/RegistrationViewModel.java
+++ app/src/main/java/ca/dal/bartertrader/RegistrationViewModel.java
@@ -1,26 +1,87 @@
 package ca.dal.bartertrader;
 
 import androidx.lifecycle.ViewModel;
-
+import android.util.Patterns;
 //private userRepository;
+
 
 public class RegistrationViewModel extends ViewModel {
 
-    RegistrationViewModel(/**UserRepository userRepository**/)
-    {
+    RegistrationViewModel(/**UserRepository userRepository**/) {
         /**this.userRepository = userRepository;**/
     }
 
-    public void registerUser( String email, String password ) {}
+    public void registerUser( String email, String username, String password ) {
+        boolean validRegistration = validateRegistration(email, username, password);
 
-    private void validateRegistration() {};
+        boolean registrationResult = false;
+        if (validRegistration)
+        {
+            //registrationResult = userRepository.registerAnonymously(email, username, password);
+        }
+        if (validRegistration && registrationResult)
+        {
+            // set live registration result callback to be a success
+            sendConfirmationEmail();
+        }
+        else
+        {
+            // fire back an error with registration.
+        }
+    }
 
-    private boolean isEmailValid() { return false; };
+    public void registrationDataChanged(String email, String username, String password)
+    {
+        if (!isEmailValid(email))
+        {
+            /** set live data to notify an invalid email **/
+        }
+        else if (!isUserNameValid(username))
+        {
+            /** set live data to notify an invalid user **/
+        }
+        else if (!isPasswordValid(password))
+        {
+            /** set live data to notify an invalid password **/
+        }
+    }
 
-    private boolean isUserNameValid() { return false; };
+    private void sendConfirmationEmail() {
+        /**TODO: send email confirmation after a valid registration **/
+    };
 
-    private boolean isPasswordValid() {return false; };
+    private boolean validateRegistration(String email, String username, String password) {
+        boolean validRegistration = isEmailValid(email)         &&
+                                    isUserNameValid(username)   &&
+                                    isPasswordValid(password)   &&
+                                    !isEmailRegistered(email);
+        return validRegistration;
+    };
 
-    private boolean isEmailRegistered() { return false; }
+    // Validates a potential email, does not check if it is already registered.
+    private boolean isEmailValid(String email) {
+        boolean emailPatternCheck = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        if (!email.isEmpty() && emailPatternCheck)
+        {
+            return true;
+        }
+        return false;
+    };
 
+    // Validates the username given for registration.
+    private boolean isUserNameValid(String username) {
+        return !username.isEmpty();
+    };
+
+    // Validates the password given for registration.
+    private boolean isPasswordValid(String password) {
+        return !password.isEmpty();
+    };
+
+    // Checks firebase to see if this email already belongs to a user.
+    private boolean isEmailRegistered(String email) {
+        boolean exists = true;
+        //exists = userRepository.emailExists(email);
+        return exists;
+    }
 }
