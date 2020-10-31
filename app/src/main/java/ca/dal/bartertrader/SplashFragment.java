bb7ca639576538c07d975f67a1f7895da5c84a36
Urmzd Mukhammadnaim <ur937874@dal.ca>
2020-10-31 19:10:49 -0400

    change start destination to splash

--- app/src/main/java/ca/dal/bartertrader/SplashFragment.java
+++ app/src/main/java/ca/dal/bartertrader/SplashFragment.java
@@ -16,6 +16,6 @@ public class SplashFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_splash, container, false);
     }
 }
