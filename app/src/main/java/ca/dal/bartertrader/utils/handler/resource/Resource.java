357751fd0cffef7489a6169230c0e0dc227c6d72
Urmzd Mukhammadnaim <ur937874@dal.ca>
2020-11-02 13:42:26 -0500

    Implementation: Create resource helper class to handle async tasks

---
+++ app/src/main/java/ca/dal/bartertrader/utils/handler/resource/Resource.java
@@ -0,0 +1,50 @@
+package ca.dal.bartertrader.utils.handler.resource;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+enum Status {
+    REJECTED,
+    FULFILLED
+}
+public class Resource<T> {
+
+    @NonNull
+    private final Status status;
+
+    @Nullable
+    private final T data;
+
+    @Nullable
+    private final Throwable error;
+
+    private Resource(@NonNull Status status, @Nullable T data, @Nullable Throwable error) {
+        this.status = status;
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> Resource<T> fulfilled(@NonNull T data) {
+        return new Resource<>(Status.FULFILLED, data, null);
+    }
+
+    public static <T> Resource<T> rejected(@NonNull Throwable error) {
+        return new Resource<>(Status.REJECTED, null, error);
+    }
+
+    @NonNull
+    public Status getStatus() {
+        return status;
+    }
+
+    @Nullable
+    public T getData() {
+        return data;
+    }
+
+    @Nullable
+    public Throwable getError() {
+        return error;
+    }
+}
+
