e94b94cbd245db34527419f51759b7770f721636
Urmzd Mukhammadnaim <ur937874@dal.ca>
2020-11-02 13:41:15 -0500

    Test: Ensure resource helper class works as expected.

---
+++ app/src/test/java/ca/dal/bartertrader/utils/handler/resource/ResourceUnitTest.java
@@ -0,0 +1,52 @@
+package ca.dal.bartertrader.utils.handler.resource;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class ResourceUnitTest {
+
+    private static String data;
+    private static Exception exception;
+
+    private static Resource<String> success;
+    private static Resource<Exception> rejected;
+
+    @BeforeClass
+    public static void onlyOnce() {
+        data = "Test";
+        exception = new IndexOutOfBoundsException();
+
+        success  = Resource.fulfilled(data);
+        rejected = Resource.rejected(exception);
+    }
+
+    @Test
+    public void getStatusReturnsEnum() {
+        assertThat(success.getStatus(), is(Status.FULFILLED));
+        assertThat(rejected.getStatus(), is(Status.REJECTED));
+    }
+
+    @Test
+    public void getDataReturnsContent() {
+        assertThat(success.getData(), is(data));
+        assertThat(rejected.getData(), is(nullValue()));
+    }
+
+    @Test
+    public void getErrorReturnsException() {
+        assertThat(success.getError(), is(nullValue()));
+        assertThat(rejected.getError(), is(exception));
+    }
+
+    @Test
+    public void enumsAreDefined() {
+        assertThat(Status.FULFILLED, is(notNullValue()));
+        assertThat(Status.REJECTED, is(notNullValue()));
+    }
+
+}
