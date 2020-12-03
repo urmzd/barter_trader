34f85fa64c249f7aa2e895581aeffcd3b1a75f4c
Elias Katsihtis <el463538@dal.ca>
2020-12-03 13:14:16 -0400

    [TESTING] Added failing instrument tests to test the functionality of the accept, decline, and review button

--- app/src/androidTest/java/ca/dal/bartertrader/ProviderOffersInstrumentedTest.java
+++ app/src/androidTest/java/ca/dal/bartertrader/ProviderOffersInstrumentedTest.java
@@ -1,11 +1,22 @@
 package ca.dal.bartertrader;
 
 
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 
+import com.google.android.material.button.MaterialButton;
+
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,6 +32,8 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.Matchers.allOf;
 
 
@@ -45,9 +58,9 @@ public class ProviderOffersInstrumentedTest {
         onView(withText("LOG IN"))
                 .perform(click());
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         onView(withId(R.id.view_provider_offers)).perform(click());
-        Thread.sleep(3000);
+        Thread.sleep(5000);
     }
 
     @Test
@@ -59,6 +72,86 @@ public class ProviderOffersInstrumentedTest {
     @Test
     public void checkProviderTitleDisplayed() {
         onView(withText("Your post: Test item")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void onAcceptClick() throws InterruptedException {
+        final TextView[] acceptText = new TextView[1];
+
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0,
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "On accept click";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.findViewById(R.id.accept_button).performClick();
+
+                        acceptText[0] = view.findViewById(R.id.accepted_text);
+
+                    }
+                }));
+        Thread.sleep(1000);
+        assertEquals(acceptText[0].getVisibility(), View.VISIBLE);
+    }
+
+    @Test
+    public void onDeclineClick() throws InterruptedException {
+        final TextView[] declineText = new TextView[1];
+
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(2,
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "On decline click";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.findViewById(R.id.decline_button).performClick();
+
+                        declineText[0] = view.findViewById(R.id.declined_text);
+
+                    }
+                }));
+        Thread.sleep(1000);
+        assertEquals(declineText[0].getVisibility(), View.VISIBLE);
+    }
+
+    @Test
+    public void onReviewClick() throws InterruptedException {
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(2,
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "On review click";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.findViewById(R.id.review_button).performClick();
+
+                    }
+                }));
+        Thread.sleep(2000);
+        onView(withText("Exchange Review Form")).check(matches(isDisplayed()));
     }
 
 }
